280be5c38afb446b70ba314859b3632ce97c75d4
nandita0401 <nanditabharambe04@gmail.com>
2025-12-08 14:22:00 -0800

    updated

--- backend/app/main.py
+++ backend/app/main.py
@@ -30,6 +30,8 @@ origins = [
     "http://127.0.0.1:3000",
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    "http://localhost:4000",
+    "http://127.0.0.1:4000",
 ]
 app.add_middleware(
     CORSMiddleware,
